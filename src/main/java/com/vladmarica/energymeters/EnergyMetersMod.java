fb8358547cb2aa1262d0b4e4b46f11a90f94a557
magcicada <l1334938030@gmail.com>
2021-08-08 23:43:43 +0800

    Update EnergyMetersMod.java

--- src/main/java/com/vladmarica/energymeters/EnergyMetersMod.java
+++ src/main/java/com/vladmarica/energymeters/EnergyMetersMod.java
@@ -19,7 +19,7 @@ public class EnergyMetersMod {
   public static final String MODID = "energymeters";
   public static final String NAME = "Forge Energy Meters";
   public static final String VERSION = "1.0.2";
-  public static final String UPDATE_CHECKER_URL = "https://gist.githubusercontent.com/vladmarica/6a3f0ef227c40d4bff4c99725fe6c3c6/raw/ebfc193ec18abe08b936f308d65f0517b04587ff/energy-meters-updates.json";
+  public static final String UPDATE_CHECKER_URL = "https://github.com/magcicada/energy-meters/blob/1.12.x/energy-meters-updates.json";
 
   public static final Logger LOGGER = LogManager.getLogger(MODID);
   public static final SimpleNetworkWrapper NETWORK =
